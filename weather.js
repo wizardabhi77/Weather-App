82fba078dac71d99b0f06ac4a06f91fb28cfc072
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-11-26 16:24:17 +0530

    Add form to get user input

--- weather.js
+++ weather.js
@@ -1,11 +1,17 @@
 
+const locationInput = document.querySelector('#location');
+const submitButton = document.querySelector('button');
+
+submitButton.addEventListener('click',handleSubmit);
 
 async function getWeather (location){
 
     try {
         const weatherData = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=AWSBXLML7WN6KRT2UV65FPXHY`);
 
-        const weather = await weatherData.json();
+        let weather = await weatherData.json();
+
+        weather = transformData(weather);
 
         console.log(weather);
 
@@ -17,4 +23,23 @@ async function getWeather (location){
      
 }
 
-getWeather('hyderabad');
+function transformData (weather) {
+
+    let currConditions = weather.currentConditions.conditions;
+    let currTime = weather.currentConditions.datetime;
+    let temperature = weather.currentConditions.temp;
+    let windSpeed = weather.currentConditions.windspeed;
+    let address = weather.resolvedAddress;
+
+    return { address,currConditions, currTime, temperature, windSpeed};
+}
+
+function handleSubmit (e) {
+    
+    e.preventDefault();
+    let location = locationInput.value;
+    getWeather(location);
+
+}
+
+
