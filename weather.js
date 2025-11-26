12a710a2e790781381f50f68e92e51263db42b4f
wizardabhi77 <vemulapalliabhinav@gmail.com>
2025-11-26 19:32:22 +0530

    add styling in css and add tempchange button

--- weather.js
+++ weather.js
@@ -2,6 +2,7 @@
 const locationInput = document.querySelector('#location');
 const submitButton = document.querySelector('button');
 const weatherCard = document.querySelector('.weather');
+const body = document.querySelector('body');
 
 submitButton.addEventListener('click',handleSubmit);
 
@@ -31,7 +32,7 @@ async function getGIF (conditions) {
 
     let img = document.createElement('img');
 
-    const gifyData = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Ym287vYVjZc5zoUoVGoGT6LnVj4sLWum&s=${conditions}&weirdness=4`);
+    const gifyData = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=Ym287vYVjZc5zoUoVGoGT6LnVj4sLWum&s=${conditions} weather&weirdness=4`);
     
     const gif = await gifyData.json();
     
@@ -61,22 +62,67 @@ function displayWeather (weather) {
     let currConditions = document.createElement('h2');
     let currTime = document.createElement('div');
     let temperature = document.createElement('p');
+    let tempChange = document.createElement('button');
     let windSpeed = document.createElement('p');
-    
+    let tempDiv = document.createElement('div');
 
-    
+    tempDiv.id = 'temp';
+    tempDiv.append(temperature);
+    tempDiv.append(tempChange);
+
+    windSpeed.id = 'wind';
+
+    tempChange.classList = 'celsius';
+    tempChange.innerHTML ='TO CELSIUS';
+
+    if(Number(weather.temperature) >= 100){
+        body.style.backgroundImage = "url('./Assets/hot.jpg')";
+    }
+    else if(Number(weather.temperature) <= 69){
+        body.style.backgroundImage = "url('./Assets/cold.jpg')";
+    }
+    else {
+        body.style.backgroundImage = "url('./Assets/sunny.jpg')";
+    }
 
     address.innerHTML = weather.address;
     currConditions.innerHTML = weather.currConditions;
-    currTime.innerHTML = weather.currTime;
-    temperature.innerHTML = weather.temperature;
-    windSpeed.innerHTML = weather.windSpeed;
+    currTime.innerHTML = `TIME : ${weather.currTime}`;
+    temperature.innerHTML = `Temperature: ${weather.temperature}`;
+    windSpeed.innerHTML = `Wind Speed : ${weather.windSpeed}`;
     
+    tempChange.addEventListener('click', (e) => {
+        
+        let temp = Number(weather.temperature);
+
+        if(e.target.classList[0] === 'celsius') {
+            
+            temp = Math.floor((temp - 32) / 1.8);
+            
+            e.target.classList = 'farenheit';
+            e.target.innerHTML = 'TO FARENHEIT';
+        }
+        else
+        {
+            temp = Number(weather.temperature);
+            e.target.classList = 'celsius';
+            e.target.innerHTML ='TO CELSIUS';
+        }
+
+        temperature.innerHTML = `Temperature: ${temp}`;
+
+    })
+
+    while(weatherCard.contains(weatherCard.firstElementChild)){
+
+        weatherCard.removeChild(weatherCard.lastElementChild);
+    
+    }
 
     weatherCard.append(address);
     weatherCard.append(currConditions);
     weatherCard.append(currTime);
-    weatherCard.append(temperature);
+    weatherCard.append(tempDiv);
     weatherCard.append(windSpeed);
 
     getGIF(weather.currConditions);
